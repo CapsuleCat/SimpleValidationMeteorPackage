45eac0beb23578247d961cecfd8188aac65c520b
Ivan Montiel <idmontie@asu.edu>
2016-02-12 11:16:01 -0700

    Update package.js

--- src/package.js
+++ src/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Core validation package for checking data, nothing more',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/idmontie/meteor-simple-validation',
+  git: 'https://github.com/CapsuleCat/SimpleValidationMeteorPackage',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: '../README.md'
